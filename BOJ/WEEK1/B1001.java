3eb509baeb78df38a968b8e8d2cb623d565c6cfc
str-leshs <0805eslee@gmail.com>
2025-09-28 00:21:50 +0900

    feat: BOJ 1001번 문제풀이-java

---
+++ BOJ/WEEK1/B1001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+class B1001 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.print(a-b);
+  }
+}
