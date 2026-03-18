b45947c9942636099acf5dd45734f38616de9cfa
str-leshs <125110572+str-leshs@users.noreply.github.com>
2026-03-18 23:58:11 +0900

    [Bronze V] Title: 사분면 고르기, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+        
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+        sc.close();
+		
+		if(X > 0) {
+			if(Y > 0) {
+				System.out.print(1);
+			} 
+			else {
+				System.out.print(4);
+			}
+		} 
+		
+		else {
+			if(Y > 0) {
+				System.out.print(2);
+			} 
+			else {
+				System.out.print(3);
+			}
+		}
+	}
+ 
+}
