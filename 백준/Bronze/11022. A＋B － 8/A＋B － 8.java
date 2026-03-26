cb9e2e6e2d9b76e3ce668c2dc61e993081afb41c
str-leshs <125110572+str-leshs@users.noreply.github.com>
2026-03-26 23:34:04 +0900

    [Bronze V] Title: A+B - 8, Time: 232 ms, Memory: 18228 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int input = Integer.parseInt(sc.nextLine());
+		
+		for(int i = 1 ; i <= input ; i++) {
+			String str = sc.nextLine();
+			int x = str.indexOf(" ");
+			int a = Integer.parseInt(str.substring(0, x));
+			int b = Integer.parseInt(str.substring(x + 1));
+			
+			System.out.printf("Case #%d: %d + %d = %d%n", i, a, b, a+b);
+		}
+		sc.close();
+	}
+}
